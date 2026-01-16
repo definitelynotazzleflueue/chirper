69b5fb8a814a54329308f2c41c8274a24d254af2
definitelynotazzleflueue <franceljohngnapi@gmail.com>
2026-01-16 16:14:42 +0800

    upto adding the route register in the auth controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\Register;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,3 +13,9 @@ Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 //to shortcut those 4 routes (store, edit, update, destroy), we can use resource route like this
 //Route::resource('chirps', ChirpController::class)
 //    ->only(['store', 'edit', 'update', 'destroy']);
+
+Route::view('/register', 'auth.register')
+        ->middleware('guest')
+        ->name('register');
+Route::post('/register', Register::class)
+        ->middleware('guest');
